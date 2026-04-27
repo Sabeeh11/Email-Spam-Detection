a6d418fec08a9a8d4ed6646da778fc9e3f823644
Sabeeh11 <98272674+Sabeeh11@users.noreply.github.com>
2026-04-27 06:47:59 +0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,29 +1,45 @@
 import pandas as pd
+import re
 import joblib
+
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-df = pd.read_csv("data/spam.csv", encoding="latin-1")
 
-df = df[["v1", "v2"]]
+def clean_text(text):
+    text = re.sub(r"[^a-zA-Z]", " ", str(text))
+    text = text.lower()
+    text = text.strip()
+    return text
+
+
+df = pd.read_csv("spam_ham_dataset.csv")
+
+df = df.drop(columns=["Unnamed: 0", "label_num"], errors="ignore")
+
+df = df[["label", "text"]]
 df.columns = ["label", "message"]
 
 df["label"] = df["label"].map({"ham": 0, "spam": 1})
+df["clean_message"] = df["message"].apply(clean_text)
+
+X = df["clean_message"]
+y = df["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    df["message"],
-    df["label"],
+    X,
+    y,
     test_size=0.2,
     random_state=42,
-    stratify=df["label"]
+    stratify=y
 )
 
-model = joblib.load("models/spam_model.pkl")
+model = joblib.load("spam_model.pkl")
 
 y_pred = model.predict(X_test)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
-print("\nClassification Report:\n")
+print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
-print("\nConfusion Matrix:\n")
+print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
